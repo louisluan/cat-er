91cacff2711e4aa845310908cf2926228ec9610e
luisluan <luisluan@qq.com>
2014-04-07 23:49:43 +0800

    R script to add packages for econometrics

---
+++ R/r-pkg-eco-fin.R
@@ -0,0 +1,71 @@
+fin=c("actuar","AmericanCallOpt","AutoSEARCH","backtest",
+     "bayesGARCH","BenfordTests","betategarch","bizdays",
+     "BurStFin","BurStMisc","CADFtest","car","ccgarch",
+     "ChainLadder","copula","CreditMetrics","crp.CSFP",
+     "data.table","dlm","dse","dyn","dynlm","egcm","ESG",
+     "evd","evdbayes","evir","extRemes","fame","fArma",
+     "fAsianOptions","fAssets","fBasics","fBonds","fCopulae",
+     "fExoticOptions","fExtremes","fgac","fGarch","fImport",
+     "FinAsym","FinTS","fMultivar","fNonlinear","fOptions",
+     "forecast","fPortfolio","fracdiff","fRegression","frmqa",
+     "fTrading","fUnitRoots","GEVStableGarch","ghyp","gmm",
+     "gogarch","GUIDE","highfrequency","IBrokers","ismev",
+     "its","lgarch","lifecontingencies","lmtest","longmemo",
+     "LSMonteCarlo","maRketSim","markovchain","MarkowitzR",
+     "MSBVAR","mvtnorm","nlme","NMOF","opefimor","OptHedging",
+     "pa","parma","PerformanceAnalytics","PIN","portfolio",
+     "portfolioSim","POT","quantmod","Rbitcoin","Rcmdr",
+     "rmgarch","RND","RQuantLib","rugarch","rwt","sandwich",
+     "sde","SharpeR","SmithWilsonYieldCurve","stochvol",
+     "stockPortfolio","strucchange","TAQMNGR","tawny",
+     "termstrc","TFX","timeDate","timeSeries","timsac","tis",
+     "TSdbi","tseries","tseriesChaos","tsfa","TTR","tvm","urca",
+     "vars","VarSwapPrice","vrtest","wavelets","waveslim",
+     "wavethresh","xts","ycinterextra","YieldCurve","Zelig","zoo")
+rep=c("animation","apsrtable","ascii","bibtex","brew",
+          "cacher","cacheSweave","exams","Hmisc","hwriter",
+               "installr","knitr","markdown","memisc","odfWeave",
+               "odfWeave.survey","pander","papeR","quantreg",
+               "R.cache","R.rsp","R2HTML","r2lh","R2PPT","R2wd",
+               "rapport","reporttools","rms","rtf","sparktex",
+               "SRPM","stargazer","suRtex","svSweave",
+               "SweaveListingUtils","table1xls","tables",
+               "TeachingSampling","texreg","tikzDevice","tth",
+               "xtable")
+eco=c("AER","aod","apt","AutoSEARCH","bayesm","betareg","boot",
+      "bootstrap","CADFtest","car","CDNmoney","censReg","crch",
+      "dlm","dse","dyn","dynlm","Ecdat","effects","erer",
+      "expsmooth","FinTS","fma","forecast","frontier","fxregime",
+      "gam","gamlss","geepack","gmm","Hmisc","ineq","intReg",
+      "its","LARF","lfe","lme4","lmtest","M
+      ASS","Matrix","Mcomp",
+      "meboot","mFilter","mfx","mgcv","mhurdle","micEcon",
+      "micEconAids","micEconCES","micEconSNQP","midasr","mlogit",
+      "mnlogit","MNP","MSBVAR","mvProbit","nlme","nnet","np",
+      "ordinal","partsm","pcse","plm","pscl","pwt","pwt8",
+      "quantreg","reldist","rms","RSGHB","sampleSelection",
+      "sandwich","sde","segmented","sem","SemiParSampleSel",
+      "simpleboot","SparseM","spatialprobit","spdep","sphet",
+      "splm","strucchange","survival","systemfit","timeSeries",
+      "truncreg","tseries","tsfa","urca","vars","VGAM","Zelig","zoo")
+ts=c("ArDec","arfima","astsa","bayesGARCH","BAYSTAR","bentcableAR","bfast","biwavelet","boot","brainwaver","bspec","CADFtest","chron","CommonTrend","cts","depmix","depmixS4","deseasonalize","dlm","dse","dtw","dyn","dynlm","Ecdat","ensembleBMA","EvalEst","events","expsmooth","fame","fArma","FeedbackTS","fGarch","FGN","FinTS","FitAR","FitARMA","FKF","fma","fNonlinear","ForeCA","forecast","fpp","fracdiff","fractal","fractalrock","fts","fUnitRoots","GeneCycle","GeneNet","GEVStableGarch","glarma","gogarch","gsarima","hydroGOF","hydroTSM","Interpol.T","its","KFAS","KFKSDS","kza","lgarch","locits","lomb","LPStimeSeries","ltsa","lubridate","MAPA","mAr","MAR1","mar1s","MARSS","Mcomp","meboot","mFilter","midasr","mondate","MSBVAR","MSwM","MTS","mvcwt","nets","nlts","nonlinearTseries","npst","paleoTS","partsm","pastecs","pdfetch","pear","perARMA","pomp","portes","psd","ptw","PVAClone","Quandl","quantspec","rdatamarket","rmaf","RMAWGEN","rmgarch","robfilter","RSEIS","Rssa","RTisean","rts","rugarch","rwt","SDD","sde","seas","season","seasonal","signalextraction","sltl","spTimer","stochvol","stsm","stsm.class","StVAR","surveillance","tempdisagg","tframe","Tides","tiger","timeDate","TimeProjection","timesboot","timeSeries","timsac","tis","tpr","TSA","TSAgg","tsbugs","TSclust","TSdbi","tsDyn","tseries","tseriesChaos","tsfa","tslars","tsModel","TSTutorial","urca","vars","wavelets","waveslim","wavethresh","WeightedPortTest","wmtsa","wq","x12","x12GUI","xts","zoo")
+install.packages(eco)
+install.packages(ts)
+install.packages(fin)
+install.packages(rep)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
